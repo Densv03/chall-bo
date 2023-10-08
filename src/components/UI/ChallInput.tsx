df13f1997d3b840f55aac7308b5e14e968edd7e9
denissuhomlin <denis.suhomlin@b2b.discount>
2023-10-08 14:35:34 +0300

    add customState hook, update ChallInput, refcator Login

--- src/components/UI/ChallInput.tsx
+++ src/components/UI/ChallInput.tsx
@@ -1,4 +1,5 @@
 import { InputThemeEnum } from "../../enums/UI/input-theme.enum";
+import { ChangeEventHandler } from "react";
 
 interface ChallInputProps {
     type: "text" | "password" | "number" | "email" | "date" | "checkbox" | "radio" | "file" | "submit" | "reset" | "button";
@@ -6,33 +7,40 @@ interface ChallInputProps {
     labelClass?: string;
     theme: InputThemeEnum;
     placeholder?: string;
+    onChange?: ChangeEventHandler<HTMLInputElement>;
+    required?: boolean;
 }
 
 function getClassnameByTheme(theme: InputThemeEnum): string {
     switch (theme) {
         case InputThemeEnum.BACKGROUND_WHITE:
-            return 'input input--white'
+            return 'input input--white';
         default:
-            return ''
+            return '';
     }
 }
 
 function getLabelClasses(labelClass?: string): string {
-    return labelClass ? `${labelClass} mb-1` : `mb-1`
+    return labelClass ? `${labelClass} mb-1` : `mb-1`;
 }
 
 function generateInputWithLabel(props: ChallInputProps): React.ReactElement {
-    const {type, label, labelClass, theme, placeholder} = props
+    const {label, labelClass} = props;
     return (<div className={'d-flex flex-column'}>
         <div className={getLabelClasses(labelClass)}>{label}</div>
-        <input type={type} className={getClassnameByTheme(theme)} placeholder={placeholder}/>
-    </div>)
+        {generateInputWithoutLabel(props)}
+    </div>);
 }
 
-// function generateInputWithoutLabel(): React.ReactElement {
-//
-// }
+function generateInputWithoutLabel(props: ChallInputProps): React.ReactElement {
+    const {type, theme, placeholder, onChange, required} = props;
+    return (<input type={type}
+                   className={getClassnameByTheme(theme)}
+                   placeholder={placeholder}
+                   onChange={onChange}
+                   required={required}/>);
+}
 
 export const ChallInput: React.FC<ChallInputProps> = (props) => {
-    return generateInputWithLabel(props);
+    return props.label ? generateInputWithLabel(props) : generateInputWithoutLabel(props);
 };
